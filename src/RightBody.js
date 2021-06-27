8c6346868e0fae9ab848249f38c34a233101c6d6
karanOO89 <gk.987680@gmail.com>
2021-06-27 15:39:37 -0600

    nav bar is added

---
+++ src/RightBody.js
@@ -0,0 +1,9 @@
+import './RightBody.scss'
+const RightBody = (props) =>{
+return(
+    <div className="rightBody">
+        rightbody
+    </div>
+)
+}
+export default RightBody
---
+++ src/SideNav.js
@@ -0,0 +1,29 @@
+import "./SideNav.scss";
+
+const SideNav = (props) => {
+  return (
+    <div className="sideNav">
+      <div className="logo">Logo</div>
+      <label className="name">
+        Karan <b>Gaba</b>
+      </label>
+      <label className="profile">Web Developer</label>
+      <div className="social">
+        <a href="https://www.linkedin.com/in/karan-gaba/" target="_blank">
+          <img src="linkedin.png" />
+        </a>
+        <a href="https://github.com/karanOO89" target="_blank">
+          <img src="github.png" />
+        </a>
+      </div>
+      <div className="loc">
+        <img src="loc1.png" />
+        <label>Calgary</label>
+      </div>
+      <div className="email">
+        <button>Email Me </button>
+      </div>
+    </div>
+  );
+};
+export default SideNav;
