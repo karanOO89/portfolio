de75c53bd3deabea3feeecc8410c14f4b23850a4
karanOO89 <gk.987680@gmail.com>
2021-06-28 00:54:52 -0600

    basic right body frame created

--- src/RightBody.js
+++ src/RightBody.js
@@ -1,9 +1,51 @@
-import './RightBody.scss'
-const RightBody = (props) =>{
-return(
+import "./RightBody.scss";
+const RightBody = (props) => {
+  return (
     <div className="rightBody">
-        rightbody
+      <div className="content">
+        <div className="intro">
+          <p className="about">
+            <b>
+              About Me
+              <br />
+              ____
+            </b>
+          </p>
+          <p className="desc">
+            I describe myself as someone who's a quick learner and persistent
+            about learning new technology, a problem solver with tools that are
+            currently available.
+          </p>
+        </div>
+        <div className="query">
+          <p><b>What I'am doing... </b></p>
+        </div>
+        <div className="midDesc">
+          <div>
+            <p>
+              I persistent about learning new technology, a problem solver with
+            </p>
+          </div>
+          <div>
+            <p>
+              I describe myself as someone who's a quick learner and persistent
+            </p>
+          </div>
+        </div>
+        <div className="bottomDesc">
+          <div>
+            <p>
+              I persistent about learning new technology, a problem solver with
+            </p>
+          </div>
+          <div>
+            <p>
+              I describe myself as someone who's a quick learner and persistent
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-)
-}
-export default RightBody
+  );
+};
+export default RightBody;
